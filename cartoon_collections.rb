84f8ebd60e3c8b4390a2e7be575336cf8a0a4155
zed <zed@kolk.me>
2020-12-23 01:00:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |item, index|
-    p "#{index+1}. #{item}"
+    print "#{index+1}. #{item}"
   end
 end
